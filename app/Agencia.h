5df7f199a4fbf9830138f3c28d106b1acc372fa1
provavelmente-o-caio <caiofecardoso@hotmail.com>
2026-06-17 11:52:09 -0300

    Mais classes estruturais, conta e agência

---
+++ app/Agencia.h
@@ -0,0 +1,13 @@
+//
+// Created by Caio Ferreira Cardoso on 17/06/26.
+//
+
+#ifndef DISTRIBUIDA_SNAPSHOT_AGENCIA_H
+#define DISTRIBUIDA_SNAPSHOT_AGENCIA_H
+
+
+class Agencia {
+};
+
+
+#endif //DISTRIBUIDA_SNAPSHOT_AGENCIA_H
---
+++ app/Conta.h
@@ -0,0 +1,13 @@
+//
+// Created by Caio Ferreira Cardoso on 17/06/26.
+//
+
+#ifndef DISTRIBUIDA_SNAPSHOT_CONTA_H
+#define DISTRIBUIDA_SNAPSHOT_CONTA_H
+
+
+class Conta {
+};
+
+
+#endif //DISTRIBUIDA_SNAPSHOT_CONTA_H
